2d4d3a8f0ee1eabc7e4073837975adebd0fe1af5
Mikayla Louie <Mikayla.Louie@D2L.com>
2021-03-26 16:04:24 -0700

    Implemented basic on-call rotation with cron job

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const { App } = require('@slack/bolt');
+const schedule = require('node-schedule');
 
 // Initializes your app with your bot token and signing secret
 const app = new App({
@@ -9,12 +10,33 @@ const app = new App({
 // Listens to incoming messages that contain "hello"
 app.message('hello', async ({ message, say }) => {
     // say() sends a message to the channel where the event was triggered
-    await say(`Hey there <@${message.user}>!`);
+    await say(`Hi there, <@${message.user}> :wave:!`);
+    // await publishMessage( "C01RC48QM60","testing");
   });
+
 
 (async () => {
   // Start your app
   await app.start(process.env.PORT || 3000);
-
   console.log('⚡️ Bolt app is running!');
+  var users = ["andrew.wong", "jenna.zhang", "candice.pang", "mikayla.louie", "rodolfo.landa", "kiki.ho", "ronan.fegan", "thomas.nakagawa"];
+  var index = 0;
+  const job = schedule.scheduleJob('0 9 * * 01', async function() {
+    try {
+      // Call the chat.postMessage method using the built-in WebClient
+      const result = await app.client.chat.postMessage({
+        // The token you used to initialize your app
+        token: process.env.SLACK_BOT_TOKEN,
+        channel: "C01RC48QM60",
+        text: `This week's automato is: <@${users[(index++ % users.length)]}> :coffee:`
+        // You could also use a blocks[] array to send richer content
+      });
+      // Print result, which includes information about the message (like TS)
+      console.log(result);
+    }
+    catch (error) {
+      console.error(error);
+    }
+  })
+
 })();
