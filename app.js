89083ee30bae0927785aeacb93f95ee4b15bfc7f
Mikayla Louie <Mikayla.Louie@D2L.com>
2021-04-13 15:25:55 -0700

    changed to using getWeek to index users

--- app.js
+++ app.js
@@ -13,33 +13,50 @@ app.message('hello', async ({ message, say }) => {
   });
 
 app.message('next', async ({ say }) => {
-  const next = (index+1) % users.length;
-  await say(`<@${users[next]}> is next :meow_dance:`);
+  var today = new Date();
+  var weekNum = (today.getWeek() +1 ) % users.length;
+
+  await say(`<@${users[weekNum]}> is \`next\` :meowcoffeespit:`);
+});
+
+app.message('help', async({say}) => {
+  await say(`Try \`on-call\`, \`next\`, or \`schedule\` for more information!`);
+});
+app.message('schedule', async ({ say }) => {
+  var today = new Date();
+  var idx = today.getWeek() % users.length;
+  var schedule = `*Here's the line up:* \n`;
+  var count = 0;
+  while (count < users.length) {
+    schedule = schedule.concat(`${++count}. ${users[idx]}`, '\n');
+    idx = ++idx % users.length;
+  }
+
+  await say(`${schedule}`);
 });
 
 app.message('on-call', async({say}) => {
-  await say(`<@${users[index % users.length]}> is on-call :meow_coffee:`);
-})
+  var today = new Date();
+  var weekNum = today.getWeek() % users.length;
 
-var users = ["andrew.wong", "jenna.zhang", "candice.pang", "mikayla.louie", "rodolfo.landa", "kiki.ho", "ronan.fegan", "thomas.nakagawa"];
-var index = 7;
+  await say(`<@${users[weekNum]}> is \`on-call\` :meow_coffee:`);
+});
+
+app.message('wakeup', async({say}) => {
+  await say(`I'm awake! :meowcoffeespit:`)
+});
+
+var users = ["mikayla.louie", "rodolfo.landa", "kiki.ho", "ronan.fegan", "thomas.nakagawa", "andrew.wong", "jenna.zhang", "candice.pang"];
 
 (async () => {
   // Start your app
   await app.start(process.env.PORT || 3000);
   console.log('⚡️ Bolt app is running!');
-  const job = schedule.scheduleJob('0 9 * * 01', async function() {
-    try {
-      index = (index+1) % users.length;
-      const result = await app.client.chat.postMessage({
-        token: process.env.SLACK_BOT_TOKEN,
-        channel: "C01RB6T20DS",
-        text: `This week's automato is: <@${users[index]}> :meow_coffee:`
-      });
-      console.log(result);
-    }
-    catch (error) {
-      console.error(error);
-    }
-  })
 })();
+
+Date.prototype.getWeek = function() {
+  var onejan = new Date(this.getFullYear(),0,1);
+  var today = new Date(this.getFullYear(),this.getMonth(),this.getDate());
+  var dayOfYear = ((today - onejan + 86400000)/86400000);
+  return Math.ceil(dayOfYear/7)
+};
